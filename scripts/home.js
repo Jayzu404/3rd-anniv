ed095e00d907fd29fa1710ab569a64685e91abe6
teejay <tjayarancinaofficial@gmail.com>
2026-02-17 01:19:17 +0800

    add: functionality to music players

--- scripts/home.js
+++ scripts/home.js
@@ -1,20 +1,56 @@
-const modal = document.querySelector('.modal');
-const envelope = document.querySelector('.envelope');
+const modal = document.querySelector(".modal");
+const envelope = document.querySelector(".envelope");
+const songBtns = document.querySelectorAll(".song-btn");
+let currentAudio = null;
+let currentButton = null;
 
-envelope.addEventListener('click', () => {
+envelope.addEventListener("click", () => {
   showModal();
 });
 
-modal.addEventListener('click', (e) => {
+modal.addEventListener("click", (e) => {
   if (e.target === modal) {
     closeModal();
   }
 });
 
 function closeModal() {
-  modal.style.display = 'none';
+  modal.style.display = "none";
 }
 
 function showModal() {
-  modal.style.display = 'flex';
+  modal.style.display = "flex";
 }
+
+songBtns.forEach((btn) => {
+  const audio = btn.closest(".song-card").querySelector("audio");
+
+  btn.addEventListener("click", () => {
+    // making sure previous audio is reset and pause
+    if (currentAudio && currentAudio !== audio) {
+      currentAudio.pause();
+      currentButton.innerText = "▶";
+      currentAudio.currentTime = 0;
+    }
+
+    currentAudio = audio;
+    currentButton = btn;
+
+    if (audio.paused) {
+      audio.play().catch((err) => {
+        btn.innerText = "▶";
+        console.error("Payback failed: ", err);
+      });
+      btn.innerText = "⏸";
+    } else {
+      audio.pause();
+      btn.innerText = "▶";
+    }
+
+    audio.addEventListener('ended', () => {
+      btn.innerText = "▶";
+      currentAudio = null;
+      currentButton = null;
+    });
+  });
+});
